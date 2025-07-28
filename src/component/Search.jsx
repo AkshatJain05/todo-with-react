a6bac7a533458bbbf4a28d32fc7be49141702e70
Akshat Jain <156729933+AkshatJain05@users.noreply.github.com>
2025-07-28 16:19:39 +0530

    Update Search.jsx

--- src/component/Search.jsx
+++ src/component/Search.jsx
@@ -20,8 +20,8 @@ function Search() {
         <input
           type="text"
           placeholder="Enter Your Task"
-          className="border-1 rounded-l-[8px] w-[250px] md:w-[450px] h-11 text-xl p-2  italic bg-blue-100 text-black" value={todo} onChange={(e)=> setTodo(e.target.value)}></input>
-        <button type="sumbit" className="border-1 active:scale-[0.9] bg-blue-500 border-black text-white text-xl p-2 pl-3 pr-3  rounded-r-[8px] cursor-pointer" >
+          className="border-1 rounded-l-[8px] w-[210px] md:w-[450px] h-11 text-xl p-2  italic bg-blue-100 text-black" value={todo} onChange={(e)=> setTodo(e.target.value)}></input>
+        <button type="sumbit" className="border-1 active:scale-[0.9] bg-blue-500 border-black text-white text-xl p-2   pl-3 pr-3  rounded-r-[8px] cursor-pointer" >
           Add
         </button>
         </form>
