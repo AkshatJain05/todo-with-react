e66af26790b17834d0d26ffd94263bb5027c99a7
Akshat Jain <156729933+AkshatJain05@users.noreply.github.com>
2025-07-28 15:46:57 +0530

    Update UI

--- src/component/Heading.jsx
+++ src/component/Heading.jsx
@@ -2,10 +2,10 @@ function Heading() {
   return (
     <>
       {/* Heading */}
-      <div className="w-full text-center  text-2xl md:text-4xl font-bold p-5  border-b-2 bg-purple-800 text-white">
+      <div className="w-full text-center  text-2xl md:text-3xl font-semibold p-2 border-b-2 bg-gray-800 text-white text-shadow-2xs text-shadow-black opacity-95  shadow-lg shadow-gray-800 border-1 border-gray-600 sticky top-0 z-10 mb-10">
         Todo List
       </div>
-    </>
+    </> 
   );
 }
 export default Heading;
